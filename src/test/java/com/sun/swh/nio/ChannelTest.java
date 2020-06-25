59e38394cc9308f17bb636140d592d5758ad5316
sunwnehongl <30740977+sunwnehongl@users.noreply.github.com>
2020-06-25 11:29:37 +0800

    nio Channel练习

---
+++ src/test/java/com/sun/swh/nio/ChannelTest.java
@@ -0,0 +1,64 @@
+package com.sun.swh.nio;
+
+import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.Buffer;
+import java.nio.ByteBuffer;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+/**
+ * @Auther: swh
+ * @Date: 2020/6/25 10:40
+ * @Description:
+ */
+public class ChannelTest {
+
+    /**
+     * 测试通过文件输入输出流的getChannel方法获取到nio的Channel
+     * 实现的是通过nio复制一个文件。
+     */
+    @Test
+    public void test() {
+        try (FileInputStream inputStream = new FileInputStream("D:\\任务.xlsx");
+             FileChannel inChannel = inputStream.getChannel();
+             FileOutputStream outputStream = new FileOutputStream("D:\\任务1.xlsx");
+             FileChannel outChannel = outputStream.getChannel()) {
+
+            ByteBuffer buffer = ByteBuffer.allocate(1024);
+            while (inChannel.read(buffer) != -1) {
+                buffer.flip();
+                outChannel.write(buffer);
+                buffer.clear();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 直接使用对应的的Channel的静态open方法来获取channel
+     */
+    @Test
+    public void test1() {
+        try (FileChannel inChannel = FileChannel.open(Paths.get("D:\\任务.xlsx"), StandardOpenOption.READ);
+             FileChannel outChannel = FileChannel.open(Paths.get("D:\\任务2.xlsx"),StandardOpenOption.READ,StandardOpenOption.WRITE,StandardOpenOption.CREATE)) {
+            MappedByteBuffer inMap = inChannel.map(FileChannel.MapMode.READ_ONLY,0,inChannel.size());
+            MappedByteBuffer outMap = outChannel.map(FileChannel.MapMode.READ_WRITE, 0, inChannel.size());
+            byte[] data = new byte[inMap.limit()];
+            inMap.get(data);
+            outMap.put(data);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
